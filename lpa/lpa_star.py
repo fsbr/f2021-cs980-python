60190079f6c71326b2aed70b88e2f44a7da7bd55
Thomas Fuller <thomasckfuller@gmail.com>
2021-11-17 12:38:24 -0500

    lpas: key sorts in lexicographical order

--- lpa/lpa_star.py
+++ lpa/lpa_star.py
@@ -1,4 +1,7 @@
 # programming lpa star (later lol)
+# this implementatin only needs to work in a grid
+# i can feel my skills increasing lol
+# tckf
 
 import numpy as np
 import heapq
@@ -15,7 +18,7 @@ class State:
         self.g = inf
         self.h = inf
         self.f = inf
-        self.rhs = 0 
+        self.rhs = inf
 
 class Edge:
     def __init(self):
@@ -28,6 +31,8 @@ class LPASTAR:
         self.start = State()
         self.goal = State()
         self.stateId = 0
+        self.V = {}
+        self.E = {}
 
     def calculate_L2(self, x1, y1, x2, y2):
         return np.sqrt((x2-x1)**2 + (y2-y1)**2)
@@ -38,6 +43,12 @@ class LPASTAR:
         x2 = state2.x
         y2 = state2.y
         return self.calculate_L2(x1,y1,x2,y2)
+
+    def convertToIdx(self, x,y):
+        return np.floor(x), np.floor(self.yMax - y)
+
+    def convertToCoordinate(self, xIdx, yIdx):
+        return "unfinished function"
 
     def readEnvironment(self, envFile):
         A = []
@@ -73,45 +84,62 @@ class LPASTAR:
         self.start.h = self.calcDist(self.start,self.goal)
         self.start.f = self.start.g + self.start.h
 
-        self.goal.g = self.calcDist(self.start, self.goal)
+        # a difference between g and ghat
+        self.goal.g = inf #self.calcDist(self.start, self.goal)
         self.goal.h = 0
         self.goal.f = self.goal.g + self.goal.h
         print("ENVIRONMENT READING fHat goal", self.start.f)
         print("ENVIRONMENT READING fHat goal", self.goal.f)
 
-
-
-
     # FOLLOWING D*-LITE PAPER
     def CalculateKey(self, state):
-        # eventually the real key has to go here
-        return state.f
+        # put that shit right on the state
+        g = state.g
+        rhs = state.rhs
+        h = state.h
+        minItem = min(g,rhs)
+        return [minItem + h, minItem]
+
     def Initialize(self):
         self.U = []             # {02}
         # {03} we did when we generate the state
         self.start.rhs = 0      # {04}
         heapq.heappush(self.U, (self.CalculateKey(self.start), self.start)) #{05}
         print("self.U", self.U)
+        sX,sY = self.convertToIdx(self.start.x, self.start.y)
+        gX,gY = self.convertToIdx(self.goal.x, self.goal.y)
+        print("sX" + str(sX) + " sY " + str(sY))
+        print("gX" + str(gX) + " gY " + str(gY))
 
     def UpdateVertex(self, u):
         pass
+
     def ComputeShortestPath(self):
         print("self.U top value", self.U)
-        while  (self.U[0][0] < self.CalculateKey(self.goal)) or (self.goal.rhs != self.goal.g):
+        print("self.U top value, broken up", self.U[0][0])
+        goalKey = self.CalculateKey(self.goal)
+        print("goal Key", goalKey)
+        while self.U[0][0] < self.CalculateKey(self.goal) or (self.goal.rhs != self.goal.g):
             u = heapq.heappop(self.U)[1]
+            print("small u is",u)
+            print("u.g", u.g)
+            print("u.rhs", u.rhs)
             if u.g > u.rhs:
                 u.g = u.rhs
-                #update vertex someday
-                #self.UpdateVertex(u)
+                # for all succ(u)
+                self.UpdateVertex(u)
             else:
                 u.g = inf
-                #{16}
-            pass
+                self.UpdateVertex(u)
 
     def LPASTAR_MAIN(self):
-        if u d!= self.start
         self.Initialize()
         self.ComputeShortestPath()
+
+
+
+
+
 if __name__ == "__main__":
     LPA = LPASTAR()
     LPA.readEnvironment("environment50_3.txt")
