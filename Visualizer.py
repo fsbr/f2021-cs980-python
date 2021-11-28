fd1ec88b34fd3120077e2d430ffa56dcf60bfd65
Thomas Fuller <thomasckfuller@gmail.com>
2021-11-28 00:40:35 -0500

    moved the visualizer into its own thing for mod_algorithm

---
+++ Visualizer.py
@@ -0,0 +1,126 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+class Visualizer:
+    def __init__(self):
+        print("Visualizer")
+
+    def plotMotionTree(self,V,E,obsMap,xMax, yMax, samples, start, goal, oldEdges):
+        fig, ax = plt.subplots()
+        print("Plotting the Motion Tree")
+        startX = start.x
+        startY = start.y
+        goalX = goal.x
+        goalY = goal.y
+        plt.plot(startX,startY, "go", markersize=10)
+        plt.plot(goalX,goalY, "ro", markersize=10)
+
+        xVec = []
+        yVec = []
+        for edge in E:
+            xVec.append(edge.source_state.x)
+            xVec.append(edge.target_state.x)
+            yVec.append(edge.source_state.y)
+            yVec.append(edge.target_state.y)
+            #ax.plot(xVec, yVec, "r-x")
+            ax.plot(xVec, yVec, "-", color="#FFA71A")
+            #ax.plot(xVec,yVec, "r")
+            xVec = []
+            yVec = []
+        #rect = patches.Rectangle( (50, 100), 10, 10, linewidth=1, edgecolor="r", facecolor = "r")
+        #ax.add_patch(rect)
+
+        xVec = []
+        yVec = []
+        #for tree in oldEdges:
+        #    for edge in tree:
+        #        xVec.append(edge.source_state.x)
+        #        xVec.append(edge.target_state.x)
+        #        yVec.append(edge.source_state.y)
+        #        yVec.append(edge.target_state.y)
+        #        #ax.plot(xVec, yVec, "r-x")
+        #        ax.plot(xVec, yVec, "-", color="#A832A4")
+        #        #ax.plot(xVec,yVec, "r")
+        #        xVec = []
+        #        yVec = []
+
+        #    # is this used?
+        #    xSolution = []
+        #    ySolution = []
+        # find goal
+        
+        xSampleVector = []
+        ySampleVector = []
+        for state in samples:
+            xSampleVector.append(state.x)
+            ySampleVector.append(state.y)
+        plt.plot(xSampleVector,ySampleVector, "o", color = "#757575", markersize=0.75)
+
+        # plotting the obstacles
+        countY = 0
+        for i in obsMap:
+            countX = 0
+            for j in i:
+                if j == 1:
+                    rect = patches.Rectangle((np.floor(countX),np.floor(yMax - countY-1)), 1,1, linewidth=1, edgecolor="k",facecolor="k")
+                    ax.add_patch(rect) 
+                countX+=1
+            countY +=1
+        plt.plot(startX,startY, "go", markersize=10)
+        plt.plot(goalX,goalY, "ro", markersize=10)
+        xVecSolu = []
+        yVecSolu = []
+        
+        foundGoal = False
+        stateOfInterest = goal
+        while stateOfInterest != start:
+            for e in E:
+                if e.target_state == stateOfInterest:
+                    foundGoal = True
+                    xVecSolu.append(e.source_state.x)
+                    yVecSolu.append(e.source_state.y)
+                    xVecSolu.append(e.target_state.x)
+                    yVecSolu.append(e.target_state.y)
+                    stateOfInterest = e.source_state
+                    plt.plot(xVecSolu, yVecSolu, color="#007FFF", linewidth=3)
+                    xVecSolu = []
+                    yVecSolu = []
+            if foundGoal == False:
+                break
+                         
+
+                    
+        plt.xlim((0,xMax))
+        plt.ylim((0,yMax))
+        plt.axis("equal")
+        plt.title("Motion Tree, Samples, and Solution Path")
+        plt.xlabel("Distance (m)")
+        plt.ylabel("Distance (m)")
+        plt.show()
+
+    def plotAttemptedEdge(self, attemptedEdges, obsMap, yMax):
+        print("plotting attempted edges")
+        print("attemptedEdges", attemptedEdges)
+        fig, ax = plt.subplots()
+        xVec = []
+        yVec = []
+        for edge in attemptedEdges:
+            xVec.append(edge.source_state.x)
+            xVec.append(edge.target_state.x)
+            yVec.append(edge.source_state.y)
+            yVec.append(edge.target_state.y)
+        ax.plot(xVec, yVec)
+        countY = 0
+        for i in obsMap:
+            countX = 0
+            for j in i:
+                if j == 1:
+                    rect = patches.Rectangle((np.floor(countX),np.floor(yMax - countY-1)), 1,1, linewidth=1, edgecolor="r",facecolor="r")
+                    ax.add_patch(rect) 
+                countX+=1
+            countY +=1
+
+        # too lazy
+        plt.xlim((0,50))
+        plt.ylim((0,50))
+        plt.show()
--- mod_algorithm.py
+++ mod_algorithm.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import heapq
 import random
+import Visualizer
 #from numba import jit
 
 import time
@@ -813,130 +814,6 @@ class BIT_STAR:
             #print("broke out") 
         return self.V, self.E 
 
-class Visualizer:
-    def __init__(self):
-        print("Visualizer")
-
-    def plotMotionTree(self,V,E,obsMap,xMax, yMax, samples, start, goal, oldEdges):
-        fig, ax = plt.subplots()
-        print("Plotting the Motion Tree")
-        startX = start.x
-        startY = start.y
-        goalX = goal.x
-        goalY = goal.y
-        plt.plot(startX,startY, "go", markersize=10)
-        plt.plot(goalX,goalY, "ro", markersize=10)
-
-        xVec = []
-        yVec = []
-        for edge in E:
-            xVec.append(edge.source_state.x)
-            xVec.append(edge.target_state.x)
-            yVec.append(edge.source_state.y)
-            yVec.append(edge.target_state.y)
-            #ax.plot(xVec, yVec, "r-x")
-            ax.plot(xVec, yVec, "-", color="#FFA71A")
-            #ax.plot(xVec,yVec, "r")
-            xVec = []
-            yVec = []
-        #rect = patches.Rectangle( (50, 100), 10, 10, linewidth=1, edgecolor="r", facecolor = "r")
-        #ax.add_patch(rect)
-
-        xVec = []
-        yVec = []
-        #for tree in oldEdges:
-        #    for edge in tree:
-        #        xVec.append(edge.source_state.x)
-        #        xVec.append(edge.target_state.x)
-        #        yVec.append(edge.source_state.y)
-        #        yVec.append(edge.target_state.y)
-        #        #ax.plot(xVec, yVec, "r-x")
-        #        ax.plot(xVec, yVec, "-", color="#A832A4")
-        #        #ax.plot(xVec,yVec, "r")
-        #        xVec = []
-        #        yVec = []
-
-        #    # is this used?
-        #    xSolution = []
-        #    ySolution = []
-        # find goal
-        
-        xSampleVector = []
-        ySampleVector = []
-        for state in samples:
-            xSampleVector.append(state.x)
-            ySampleVector.append(state.y)
-        plt.plot(xSampleVector,ySampleVector, "o", color = "#757575", markersize=0.75)
-
-        # plotting the obstacles
-        countY = 0
-        for i in obsMap:
-            countX = 0
-            for j in i:
-                if j == 1:
-                    rect = patches.Rectangle((np.floor(countX),np.floor(yMax - countY-1)), 1,1, linewidth=1, edgecolor="k",facecolor="k")
-                    ax.add_patch(rect) 
-                countX+=1
-            countY +=1
-        plt.plot(startX,startY, "go", markersize=10)
-        plt.plot(goalX,goalY, "ro", markersize=10)
-        xVecSolu = []
-        yVecSolu = []
-        
-        foundGoal = False
-        stateOfInterest = goal
-        while stateOfInterest != start:
-            for e in E:
-                if e.target_state == stateOfInterest:
-                    foundGoal = True
-                    xVecSolu.append(e.source_state.x)
-                    yVecSolu.append(e.source_state.y)
-                    xVecSolu.append(e.target_state.x)
-                    yVecSolu.append(e.target_state.y)
-                    stateOfInterest = e.source_state
-                    plt.plot(xVecSolu, yVecSolu, color="#007FFF", linewidth=3)
-                    xVecSolu = []
-                    yVecSolu = []
-            if foundGoal == False:
-                break
-                         
-
-                    
-        plt.xlim((0,xMax))
-        plt.ylim((0,yMax))
-        plt.axis("equal")
-        plt.title("Motion Tree, Samples, and Solution Path")
-        plt.xlabel("Distance (m)")
-        plt.ylabel("Distance (m)")
-        plt.show()
-
-    def plotAttemptedEdge(self, attemptedEdges, obsMap, yMax):
-        print("plotting attempted edges")
-        print("attemptedEdges", attemptedEdges)
-        fig, ax = plt.subplots()
-        xVec = []
-        yVec = []
-        for edge in attemptedEdges:
-            xVec.append(edge.source_state.x)
-            xVec.append(edge.target_state.x)
-            yVec.append(edge.source_state.y)
-            yVec.append(edge.target_state.y)
-        ax.plot(xVec, yVec)
-        countY = 0
-        for i in obsMap:
-            countX = 0
-            for j in i:
-                if j == 1:
-                    rect = patches.Rectangle((np.floor(countX),np.floor(yMax - countY-1)), 1,1, linewidth=1, edgecolor="r",facecolor="r")
-                    ax.add_patch(rect) 
-                countX+=1
-            countY +=1
-
-        # too lazy
-        plt.xlim((0,50))
-        plt.ylim((0,50))
-        plt.show()
-
 if __name__ == "__main__":
     #vertices = open("vertices.txt","w")
     costFile = open("costs.csv", "w")
@@ -952,8 +829,8 @@ if __name__ == "__main__":
     #fileList = ["test_environments/grid_envs_changing10/environment10_B_5.txt", "test_environments/grid_envs_changing10/environment10_A_5.txt"]
 
     # instance 7 is interesting
-    #fileList = ["test_environments/grid_envs_changing/environment50_B_7.txt", "test_environments/grid_envs_changing/environment50_A_7.txt"]
-    fileList = ["test_environments/grid_envs_changing/environment50_A_7.txt", "test_environments/grid_envs_changing/environment50_B_7.txt"]
+    fileList = ["test_environments/grid_envs_changing/environment50_B_7.txt", "test_environments/grid_envs_changing/environment50_A_7.txt"]
+    #fileList = ["test_environments/grid_envs_changing/environment50_A_7.txt", "test_environments/grid_envs_changing/environment50_B_7.txt"]
     BS.readEnvironment(fileList[0], False)
     #BS.readEnvironment("test_environments/grid_envs50/environment50_3.txt")
     #BS.readEnvironment("test_environments/grid_envs1000/environment1000_3.txt")
@@ -988,7 +865,7 @@ if __name__ == "__main__":
     #    print("x", i.x)
     #    print("y", i.y)
 
-    gv = Visualizer()
+    gv = Visualizer.Visualizer()
     gv.plotMotionTree(V,E,BS.obs,BS.xMax, BS.yMax,BS.Xsamples,BS.start, BS.goal, BS.oldE)
     #gv.plotAttemptedEdge(BS.dbgAttemptedEdgeList, BS.obs, BS.yMax)
 
